dc376f431f2563c27dcab6c22bc9612f6de0c608
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-01-09 11:16:36 +0000

    MNT: Re-rendered with conda-smithy 1.7.0

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -49,7 +49,7 @@ source run_conda_forge_build_setup
 yum install -y xorg-x11-server-Xvfb
 
 
-# Embarking on 3 case(s).
+# Embarking on 4 case(s).
     set -x
     export CONDA_PY=27
     set +x
@@ -64,6 +64,12 @@ yum install -y xorg-x11-server-Xvfb
 
     set -x
     export CONDA_PY=35
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
+
+    set -x
+    export CONDA_PY=36
     set +x
     conda build /recipe_root --quiet || exit 1
     upload_or_check_non_existence /recipe_root conda-forge --channel=main || exit 1
